e3286844f989c1c46b8d37cc9b9834b30dcf2ee1
Enrique Jimenez <ejimenez2292@gmail.com>
2022-05-11 19:45:17 -0500

    added session and cookies

--- server.js
+++ server.js
@@ -9,11 +9,26 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'process.env.SESSION_SECRET',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 
 
 app.use(express.json());
