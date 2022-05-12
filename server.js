bd22e7d623366ba06404eeb42c2b6ef3202b1470
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-11 19:29:31 -0500

    test commit

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
 //const routes = require('./controllers');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
