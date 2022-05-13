66600b444ed7d2d48114f1440d5b6d2292c4b66b
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-12 23:33:36 -0500

    almost getting handlebars figured out

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
-withAuth = require('../../utils/auth');
+//withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -76,7 +76,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', withAuth, (req, res) => {
+router.post('/', /*withAuth,*/ (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
@@ -90,7 +90,7 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.put('/upvote', withAuth, (req, res) => {
+router.put('/upvote', /*withAuth,*/ (req, res) => {
   // custom static method created in models/Post.js
   Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
@@ -100,7 +100,7 @@ router.put('/upvote', withAuth, (req, res) => {
     });
 });
 
-router.put('/:id', withAuth, (req, res) => {
+router.put('/:id', /*withAuth,*/ (req, res) => {
   Post.update(
     {
       title: req.body.title
@@ -124,7 +124,7 @@ router.put('/:id', withAuth, (req, res) => {
     });
 });
 
-router.delete('/:id', withAuth, (req, res) => {
+router.delete('/:id', /*withAuth,*/ (req, res) => {
   console.log('id', req.params.id);
   Post.destroy({
     where: {
