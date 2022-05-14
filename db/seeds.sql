8bd4d8f264fedc6f9b1a652459821fbdc559d3e3
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-05-14 00:03:14 -0500

    feature setup+3 users

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO user
+(username , email, password)
+VALUES
+(gothprincess, xxdarknessxx@aol.com, rapunzel)
+(MCR4evaa, Ilovegerardway@gmail.com, blackparade)
+(horseGORL, countrymatters@yahoo.com, YeeHaCountry)
+(QueenCEO, electricdaisycarnicval@ceo.com, corporaTeTake0ver)
+
+
+INSERT INTO post/* Remember to update with spotify api when implemented */
+(title , post_url, user_id )
+VALUE
+
