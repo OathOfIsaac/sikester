83206b6f4458917df429be7c48283b07b2bc63ba
Enrique Jimenez <ejimenez2292@gmail.com>
2022-05-13 23:09:44 -0500

    added posts js, still needs testing

---
+++ public/js/add-posts.js
@@ -0,0 +1,25 @@
+async function newPost(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const spotify_url = document.querySelector('input[name="spotify_url"]').value;
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            spotify_url
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/home');
+    } else {
+        alert(response.statusText)
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newPost);
