29d3d30d68caa9beb252a5b32f7f213d6b178a20
Enrique Jimenez <ejimenez2292@gmail.com>
2022-05-15 20:50:50 -0500

    got message to post with socket, saving to move to laptop

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const exphbs = require('express-handlebars');
-
-
+const http = require('http');
 
 const routes = require('./controllers');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+
 
 const sess = {
     secret: 'process.env.SESSION_SECRET',
@@ -40,7 +43,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use(routes);
 app.use(require('./controllers/'));
 
+
+//socket.io
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/main');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  //new user
+  socket.emit('message', 'Welcome to Sikester!');
+
+  //user has joined
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  //user has left
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat!')
+  })
+});
+
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on port 3001!'));
+  server.listen(PORT, () => console.log('Now listening on port 3001!'));
 });
